c924f5de3d0b2cbd13d6ddc912f12a2d81f8ed05
Hugh Loughrey <hugh.loughrey@purplebricks.com>
2020-01-05 21:22:27 +0000

    Scaffold UI development env

---
+++ ui/src/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Leaflet from 'leaflet';
+import Map from './components/molecules/map';
+
+ReactDOM.render(<Map />, document.getElementById('app'));
