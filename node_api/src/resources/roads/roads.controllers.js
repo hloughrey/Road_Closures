b76fc55382ff749e9b9124331d3ccf0ff15c7075
Hugh Loughrey <hugh.loughrey@purplebricks.com>
2020-01-19 21:10:54 +0000

    Create getRoads endpoint
    Include road name & get by bounding box filters

--- node_api/src/resources/roads/roads.controllers.js
+++ node_api/src/resources/roads/roads.controllers.js
@@ -1,9 +1,34 @@
-function getRoads(req, res) {
-    console.log(req.params);
-    console.log(req.query);
+import { Pool } from 'pg';
 
-    res.send(200).json({
-        msg: 'Success',
+const pool = new Pool({
+    user: 'admin',
+    host: 'db',
+    database: 'roads',
+    password: 'password123',
+    port: 5432,
+});
+
+function filterRoads(req) {
+    if (req.query.bbox) {
+        // TODO: Add some validation to check bbox is list of numbers
+        return `SELECT identifier, roadnumber, name1, formofway, startnode, endnode, function, ST_AsGeoJSON(geom) as geom
+        FROM roads.roadlink
+        WHERE ST_Within(geom, ST_Transform(ST_MakeEnvelope(${req.query.bbox}, 4326), 27700));`;
+    }
+
+    if (req.query.name) {
+        return `SELECT identifier, roadnumber, name1, formofway, startnode, endnode, function, ST_AsGeoJSON(geom)
+        FROM roads.roadlink
+        WHERE name1 ILIKE '${req.query.name}%';`;
+    }
+}
+
+function getRoads(req, res) {
+    pool.query(filterRoads(req), (err, results) => {
+        if (err) {
+            console.error(err);
+        }
+        res.json({ data: results.rows });
     });
 }
 
--- node_api/src/server.js
+++ node_api/src/server.js
@@ -1,10 +1,21 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import config from './config';
 import roadsRouter from './resources/roads/roads.router';
 
 export const app = express();
 
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+
 app.use('/api/roads', roadsRouter);
+app.use('/api', (req, res) => {
+    res.json({ info: 'Node.js, Express, and Postgres API' });
+});
 
 export const start = async () => {
     try {
