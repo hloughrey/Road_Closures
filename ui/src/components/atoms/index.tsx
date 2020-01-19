1c128056d1dfe1305bbb1fb9ce4c6d08e09ba74d
Hugh Loughrey <hugh.loughrey@purplebricks.com>
2020-01-19 18:00:30 +0000

    Get bounds of map

--- ui/src/components/atoms/index.tsx
+++ ui/src/components/atoms/index.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLeaflet } from 'react-leaflet';
 
 function HelloWorld() {
-    return <h1>Hello Hugh</h1>;
+    const { map } = useLeaflet();
+    const [bounds, setBounds] = useState({});
+
+    useEffect(() => {
+        const eventHandler = event => {
+            const { _southWest, _northEast } = event.target.getBounds();
+            console.log(_southWest, _northEast);
+            setBounds(event.target.getBounds());
+        };
+        map.on('moveend', eventHandler);
+
+        return () => {
+            map.off('moveend', eventHandler); // Remove event handler to avoid creating multiple handlers
+        };
+    }, [setBounds, map]);
+
+    return (
+        // Use bounds for whatever you need
+        <div>
+            Lat: {bounds.lat}; long: {bounds.lng}
+        </div>
+    );
 }
 
 export default HelloWorld;
--- ui/src/components/molecules/map/index.tsx
+++ ui/src/components/molecules/map/index.tsx
@@ -1,21 +1,30 @@
-import React from 'react';
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import React, { useState } from 'react';
+import { Map, TileLayer, Rectangle } from 'react-leaflet';
+import HelloWorld from '../../atoms';
 
-const position = [51.505, -0.09];
+const bounds = [
+    [52.02883848153629, -2.788467407226563],
+    [52.071065406906634, -2.630538940429688]
+];
+
 function map() {
+    const [viewPort, setViewPort] = useState({
+        center: [52.05, -2.71],
+        zoom: 13
+    });
+
+    function handleViewportChange(e) {
+        setViewPort(e);
+    }
+
     return (
-        <Map center={position} zoom={13}>
+        <Map onViewportChanged={handleViewportChange} viewport={viewPort}>
+            <HelloWorld />
             <TileLayer
+                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             />
-            <Marker position={position}>
-                <Popup>
-                    A pretty CSS3 popup.
-                    <br />
-                    Easily customizable.
-                </Popup>
-            </Marker>
+            <Rectangle bounds={bounds} color={'red'} />
         </Map>
     );
 }
